45d37487b9a77f630427064eadf5a2c5c6243888
BertIsTheWurd <bertram.pinstrup.jensen@outlook.com>
2026-04-19 23:54:16 +0200

    Kuwahara Filter

--- P4 Project/Assets/Scripts/KuwaharaFilter.hlsl
+++ P4 Project/Assets/Scripts/KuwaharaFilter.hlsl
@@ -1,10 +1,14 @@
+Texture2D _BlitTexture;
+SamplerState sampler_BlitTexture;
+
 struct region
 {
     float3 mean;
     float variance;
 };
 
-region calcRegion(int2 lower, int2 upper, int samples, float UV)
+
+region calcRegion(int2 lower, int2 upper, int samples, float2 UV, float2 texelSize)
 {
     region r;
     
@@ -15,7 +19,11 @@ region calcRegion(int2 lower, int2 upper, int samples, float UV)
     {
         for (int y = lower.y; y <= upper.y; y++)
         {
-            
+            float2 offset = float2(texelSize.x * x, texelSize.y * y);
+            float3 tex = _BlitTexture.Sample(sampler_BlitTexture, UV + offset).rgb;
+
+            sum += tex;
+            squareSum += tex * tex;
         }
     }
     
@@ -27,11 +35,21 @@ region calcRegion(int2 lower, int2 upper, int samples, float UV)
     return r;
 }
 
-void Kuwahara(float4 UV, Texture2D Tex, SamplerState Samp, int KernelSize, out float4 Color)
+void Kuwahara_float(float2 UV, float2 TexelSize, float KernelSize, out float4 Color)
 {
     int upper = (KernelSize - 1) / 2;
     int lower = -upper;
     int samples = (upper + 1) * (upper + 1);
-    
-    
+
+    region r1 = calcRegion(int2(lower, lower), int2(0, 0),         samples, UV, TexelSize);
+    region r2 = calcRegion(int2(0, lower),     int2(upper, 0),     samples, UV, TexelSize);
+    region r3 = calcRegion(int2(lower, 0),     int2(0, upper),     samples, UV, TexelSize);
+    region r4 = calcRegion(int2(0, 0),         int2(upper, upper), samples, UV, TexelSize);
+
+    region best = r1;
+    if (r2.variance < best.variance) best = r2;
+    if (r3.variance < best.variance) best = r3;
+    if (r4.variance < best.variance) best = r4;
+
+    Color = float4(best.mean, 1.0);
 }
