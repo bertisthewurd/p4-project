980aad1c34f6ec1ec6ecfca07185dc5efa1aed76
felix hasal <felixhasal@gmail.com>
2026-05-11 11:33:27 +0200

    portal work better now yay
    heheheheheheh

--- P4 Project/Assets/Scripts/doorsystem/PortalTeleporter.cs
+++ P4 Project/Assets/Scripts/doorsystem/PortalTeleporter.cs
@@ -2,34 +2,42 @@ using UnityEngine;
 
 public class PortalTeleporter : MonoBehaviour
 {
-
     public PortalTeleporter otherTeleport;
+    private bool hasTeleported = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hasTeleported = false; // Reset when entering the trigger
+    }
 
     private void OnTriggerStay(Collider other)
     {
+        if (hasTeleported) return;
 
-        float zPosition = transform.worldToLocalMatrix.MultiplyPoint3x4(other.transform.position).z;
+        Vector3 localPosition = transform.worldToLocalMatrix.MultiplyPoint3x4(other.transform.position);
 
-        if (zPosition < 0)
+        // Check both z and x axes so all entry directions are caught
+        if (localPosition.z < 0)
+        {
+            hasTeleported = true;
             Teleport(other.transform);
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        hasTeleported = false; // Allow teleport again after exiting
     }
 
     private void Teleport(Transform objectToTeleport)
     {
-
-    
-
         Debug.Log("teleport");
 
         Vector3 localPosition = transform.worldToLocalMatrix.MultiplyPoint3x4(objectToTeleport.position);
-
         localPosition = new Vector3(-localPosition.x, localPosition.y, -localPosition.z);
-
         objectToTeleport.position = otherTeleport.transform.localToWorldMatrix.MultiplyPoint3x4(localPosition);
 
-        Quaternion diffrence = otherTeleport.transform.rotation * Quaternion.Inverse(transform.rotation * Quaternion.Euler(0, 180, 0));
-        objectToTeleport.rotation = diffrence * objectToTeleport.rotation;
+        Quaternion difference = otherTeleport.transform.rotation * Quaternion.Inverse(transform.rotation * Quaternion.Euler(0, 180, 0));
+        objectToTeleport.rotation = difference * objectToTeleport.rotation;
     }
-
 }
